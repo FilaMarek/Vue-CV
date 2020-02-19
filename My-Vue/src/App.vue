5df042324597a95b97ea5fd11c6a845d1d57757b
FilaMarek <48298031+FilaMarek@users.noreply.github.com>
2020-02-19 14:10:42 +0100

    copyright year

--- My-Vue/src/App.vue
+++ My-Vue/src/App.vue
@@ -2,15 +2,35 @@
   <div class="jk-container">
     <Header></Header>
     <router-view></router-view>
+    <Footer :currYear="currYear"></Footer>
   </div>
 </template>
 
 <script>
+let currYear = 0;
+
 import Header from "./Shared/Header.vue";
+import Footer from "./Shared/Footer.vue";
 
 export default {
+  data: function() {
+    return {
+      currYear: "0"
+    };
+  },
+
   components: {
-    Header: Header
+    Header: Header,
+    Footer: Footer
+  },
+  created: function() {
+    this.CurrentYears();
+  },
+  methods: {
+    CurrentYears() {
+      currYear = new Date().getFullYear();
+      this.currYear = currYear;
+    }
   }
 };
 </script>
